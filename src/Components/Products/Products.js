8e07f71c02e2c86c57ae807a0643adf87a66cfe8
Srikanth <srikanthreddyuppaluru@gmail.com>
2021-06-17 09:05:22 +0530

    design rework

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -2,32 +2,13 @@ import React from 'react'
 import { Container, Row, Col, Button, Carousel} from 'react-bootstrap';
 import back from './Design.jpg';
 import './Products.css';
-// import $ from 'js/jquery.hislide.js';
+
 
 function Products() {
     return (
-        // $('.slide').hiSlide();
+
         <>
-        {/* <div className="body">
-            <div className="slide hi-slide">
-                <div className="hi-prev"></div>
-                <div className="hi-next"></div>
-                 <ListGroup as="ul" className="ul">
-                    <ListGroup.Item as="li" active className="li">
-                        <img src={back} alt="First slide"/>
-                    </ListGroup.Item>
-                    <ListGroup.Item as="li" className="li">
-                        <img src={back} alt="First slide"/>
-                        </ListGroup.Item>
-                    <ListGroup.Item as="li" disabled className="li">
-                    <img src={back} alt="First slide"/>
-                    </ListGroup.Item>
-                    <ListGroup.Item as="li" className="li">
-                    <img src={back} alt="First slide"/>
-                        </ListGroup.Item>
-                    </ListGroup>
-            </div>
-            </div> */}
+        
 <div className="data-slide">
             <Carousel>
   <Carousel.Item>
