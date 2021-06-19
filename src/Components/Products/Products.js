438e22ea845e12356ae925e9f4ab6d4e1070902c
Srikanth <srikanthreddyuppaluru@gmail.com>
2021-06-19 22:32:14 +0530

    design rework

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -2,75 +2,46 @@ import React from 'react'
 import { Container, Row, Col, Button, Carousel} from 'react-bootstrap';
 import back from './Design.jpg';
 // import $ from 'js/jquery.hislide.js';
+import './Products.css'
 
 function Products() {
     return (
-        // $('.slide').hiSlide();
+
         <>
-        {/* <div className="body">
-            <div className="slide hi-slide">
-                <div className="hi-prev"></div>
-                <div className="hi-next"></div>
-                 <ListGroup as="ul" className="ul">
-                    <ListGroup.Item as="li" active className="li">
-                        <img src={back} alt="First slide"/>
-                    </ListGroup.Item>
-                    <ListGroup.Item as="li" className="li">
-                        <img src={back} alt="First slide"/>
-                        </ListGroup.Item>
-                    <ListGroup.Item as="li" disabled className="li">
-                    <img src={back} alt="First slide"/>
-                    </ListGroup.Item>
-                    <ListGroup.Item as="li" className="li">
-                    <img src={back} alt="First slide"/>
-                        </ListGroup.Item>
-                    </ListGroup>
-            </div>
-            </div> */}
 
-            <Carousel>
+<div className="data-slide">
+            <Carousel className="products-slide">
   <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src={back}
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h3>Deer Wallets</h3>
-      <p>Dry your hand off in the durable 430gsm 
+
+
+      <h3 className="products-heading">Deer Wallets</h3>
+      <p className="products-content">Dry your hand off in the durable 430gsm 
           softness of the 100% sheep chaimois, A superb everyday 
           towel good quality at great value.</p>
-    </Carousel.Caption>
+
   </Carousel.Item>
   <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src={back}
-      alt="Second slide"
-    />
 
-    <Carousel.Caption>
-    <h3>Deer Wallets</h3>
-      <p>Dry your hand off in the durable 430gsm 
+
+
+    <h3 className="products-heading">Deer Wallets</h3>
+      <p className="products-content">Dry your hand off in the durable 430gsm 
           softness of the 100% sheep chaimois, A superb everyday 
           towel good quality at great value.</p>
-    </Carousel.Caption>
+
   </Carousel.Item>
   <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src={back}
-      alt="Third slide"
-    />
 
-    <Carousel.Caption>
-      <h3>Deer Wallets</h3>
-      <p>Dry your hand off in the durable 430gsm 
+
+   
+      <h3 className="products-heading">Deer Wallets</h3>
+      <p className="products-content">Dry your hand off in the durable 430gsm 
           softness of the 100% sheep chaimois, A superb everyday 
           towel good quality at great value.</p>
-    </Carousel.Caption>
+ 
   </Carousel.Item>
 </Carousel>
+</div>
         </>
     )
 }
--- src/Components/Stepper/Stepper.js
+++ src/Components/Stepper/Stepper.js
@@ -1,19 +1,37 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Stepper.css';
+import stepperimage from './points.png';
 
 function Stepper() {
+const [showPathContent,setShowPathContent]=useState('');
 
     return (
-
         <>
           <div className="timeline-back">
               <div className="main align-content-center align-items-center">
-                  <p className="stepper c"></p>
-                  <p className="stepper"></p>
-                  <p className="stepper"></p>
-                  <p className="stepper"></p>
-                  <p className="stepper"></p>
-                  <p className="stepper c"></p>
+                  <div className="stepper c">
+                  </div>
+                  <div className="stepper d-flex" onMouseLeave={()=>setShowPathContent('')} onClick={()=>setShowPathContent('1')} onMouseEnter={()=>setShowPathContent('1')}>
+                      { showPathContent === '1' ?  <><p className="stepper-content">We combine time honoured tanning techniques with the most 
+                      modern production processes to deliver the finest quality Natural Chamois.</p>
+                      <img src={stepperimage} className="stepper-img" /></> :null}
+                  </div>
+                  <div className="stepper d-flex" onMouseLeave={()=>setShowPathContent('')} onClick={()=>setShowPathContent('2')} onMouseEnter={()=>setShowPathContent('2')}>
+                      { showPathContent === '2' ?  <><p className="stepper-content">We combine time honoured tanning techniques with the most 
+                  modern production processes to deliver the finest quality Natural Chamois.</p>
+                      <img src={stepperimage} className="stepper-img" /></> :null}
+                  </div>
+                  <div className="stepper d-flex" onMouseLeave={()=>setShowPathContent('')} onClick={()=>setShowPathContent('3')} onMouseEnter={()=>setShowPathContent('3')}>
+                      { showPathContent === '3' ?  <><p className="stepper-content">We combine time honoured tanning techniques with the most 
+                  modern production processes to deliver the finest quality Natural Chamois.</p>
+                      <img src={stepperimage} className="stepper-img" /></> :null}
+                  </div>
+                  <div className="stepper d-flex" onMouseLeave={()=>setShowPathContent('')} onClick={()=>setShowPathContent('4')} onMouseEnter={()=>setShowPathContent('4')}>
+                      { showPathContent === '4' ?  <><p className="stepper-content">We combine time honoured tanning techniques with the most 
+                  modern production processes to deliver the finest quality Natural Chamois.</p>
+                      <img src={stepperimage} className="stepper-img" /></> :null}
+                  </div>
+                  <div className="stepper c"></div>
                 </div>
           </div>
        </>
