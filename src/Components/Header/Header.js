166707831ea946256b48ec8c20f1969203bdef93
Srikanth <srikanthreddyuppaluru@gmail.com>
2021-06-22 14:27:14 +0530

    design update

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,23 +1,39 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Container, Row, Col, Button} from 'react-bootstrap';
+import React,{useState} from 'react'
+import {Container, Row,Col } from 'react-bootstrap';
 import './Header.css';
 import { FaGripLines } from "react-icons/fa";
 import { FcCloseUpMode, FcBiomass } from "react-icons/fc";
+import { FaTimes } from "react-icons/fa";
 
-
-function header() {
+function Header() {
+  
+  const [menuShow, setMenuShow] = useState(false);
+  console.log(menuShow)
     return (
         <>
-<Container className="top-bar">
-  <Row>
-    <Col className="header-col"><FcCloseUpMode  className="icon"/></Col>
-    <Col className="header-col"><FaGripLines  className="icon"/> </Col>
-    <Col className="header-col"><FcBiomass  className="icon"/></Col>
-  </Row>
-</Container>
+        <Container className="top-bar">
+          <Row>
+            <Col className="header-col"><FcCloseUpMode  className="icon"/></Col>
+            <Col className="header-col"><FaGripLines  className="icon" onClick={()=>setMenuShow(true)} /> </Col>
+            <Col className="header-col"><FcBiomass  className="icon"/></Col>
+          </Row>
+        </Container>
+        {
+          menuShow && 
+          <div className="top-bars"  onClick={()=>setMenuShow(false)}>
+            <Row>
+                <Col className="header-col"><FcCloseUpMode  className="icon"/></Col>
+                <Col className="header-col"><FaTimes  className="icon"/> </Col>
+                <Col className="header-col"><FcBiomass  className="icon"/></Col>
+            </Row>
+            <Row>
+                <Col className="menu-col1 col-4"></Col>
+                <Col className="menu-col2 col-8"> </Col>
+            </Row>
+          </div>
+        }
         </>
     )
 }
+export default Header
 
-export default header
