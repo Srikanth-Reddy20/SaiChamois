f7365202c6dbf2fad7687df02e6f54b880a53e19
Srikanth <srikanthreddyuppaluru@gmail.com>
2021-07-04 20:10:57 +0530

    update

--- src/Components/Slider2/Slider2.js
+++ src/Components/Slider2/Slider2.js
@@ -1,31 +1,36 @@
-import React from 'react'
-import sliderimage from './Leather-1.jpg';
-import { Container, Row, Col, Button} from 'react-bootstrap';
-import './Slider2.css';
-import upperimage from './points.png';
+import React from "react";
+import sliderimage from "./Leather-1.jpg";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import "./Slider2.css";
+import upperimage from "./points.png";
 
 function Slider2() {
-    return (
-        <>
-<Container className="slider2-container p-0" fluid>
-   <div className="d-md-flex">
-    <Col className="slider2-image">
-    <div className="bx">
-      <div className="bx1">
-          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-         <div className="triangle-bottomright"></div>
-      </div>
-      <div className="bx2">
-         <img src={upperimage} alt="upper-image" className="slide2-icon" />
-      </div>
-      </div>
-      <div className="triangle-white"></div>
-    </Col>
-   </div>
-</Container>
-        </>
-    )
+  return (
+    <>
+      <Container className="slider2-container p-0" fluid>
+        <div className="d-md-flex">
+          <Col className="slider2-image">
+            <div className="bx">
+              <div className="bx1">
+                Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+                Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+                Lorem ipsum
+                <div className="triangle-bottomright"></div>
+              </div>
+              <div className="bx2">
+                <img
+                  src={upperimage}
+                  alt="upper-image"
+                  className="slide2-icon"
+                />
+              </div>
+            </div>
+            <div className="triangle-white"></div>
+          </Col>
+        </div>
+      </Container>
+    </>
+  );
 }
 
-export default Slider2
+export default Slider2;
