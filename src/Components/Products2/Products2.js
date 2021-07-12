5783bc8ea25905ee9353b4c6a9a306843e1d1519
Srikanth <srikanthreddyuppaluru@gmail.com>
2021-07-12 23:30:05 +0530

    update

--- src/Components/Products2/Products2.js
+++ src/Components/Products2/Products2.js
@@ -6,25 +6,38 @@ import "./Products2.css";
 function Products2() {
     return (
         <>
-            <div className="data-slide2" id="products2">
-        <Carousel className="products-slide">
+           <div className="products-marquee">
+            <marquee className="products-head">
+                &bull; PRODUCTS &bull; PRODUCTS &bull; PRODUCTS 
+                &bull; PRODUCTS
+                &bull; PRODUCTS &bull; PRODUCTS &bull; PRODUCTS &bull; PRODUCTS
+                &bull; PRODUCTS &bull; PRODUCTS
+            </marquee>
+        </div>
+      <div className="data-slide2" id="products">
+        <div className="circle1"></div>
+        <div className="circle2"></div>
+        <div className="circle3"></div>
+        <div className="circle4"></div>
+        <div className="circle5"></div>
+        <Carousel className="products-slide2">
           <Carousel.Item>
-            <h3 className="products-heading">Deer Wallets</h3>
-            <p className="products-content">
+            <h3 className="products-heading2">Deer Wallets</h3>
+            <p className="products-content2">
               Dry your hand off in the durable 430gsm softness of the 100% sheep
               chaimois, A superb everyday towel good quality at great value.
             </p>
           </Carousel.Item>
           <Carousel.Item>
-            <h3 className="products-heading">Deer Wallets</h3>
-            <p className="products-content">
+            <h3 className="products-heading2">Deer Wallets</h3>
+            <p className="products-content2">
               Dry your hand off in the durable 430gsm softness of the 100% sheep
               chaimois, A superb everyday towel good quality at great value.
             </p>
           </Carousel.Item>
           <Carousel.Item>
-            <h3 className="products-heading">Deer Wallets</h3>
-            <p className="products-content">
+            <h3 className="products-heading2">Deer Wallets</h3>
+            <p className="products-content2">
               Dry your hand off in the durable 430gsm softness of the 100% sheep
               chaimois, A superb everyday towel good quality at great value.
             </p>
