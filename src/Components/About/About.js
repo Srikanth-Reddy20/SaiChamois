1752e0e9a35b106c9e13a07d2136b926be4cbbbb
Srikanth <srikanthreddyuppaluru@gmail.com>
2021-10-12 22:36:24 +0530

    update

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -24,77 +24,52 @@ function About() {
                         <img src={rightimg} className="rgt-img" />
                     </div>
                 </div>
-                <p className="para2"> New Zealand Origin Bovine and Ovine 
-                Skins Tannned with Marine Fish Oils
-                in Traditional Processing Methods. New Zealand Origin Bovine and Ovine 
-                Skins Tannned with Marine Fish Oils
-                in Traditional Processing Methods. New Zealand Origin Bovine and Ovine 
-                Skins Tannned with Marine Fish Oils
-                in Traditional Processing Methods. New Zealand Origin Bovine and Ovine 
-                Skins Tannned with Marine Fish Oils
-                in Traditional Processing Methods.</p>
+                <p className="para2"> We are a family-owned company established in the year 2000, Primarily 
+                processing Natural Chamois Leathers from 100 % New Zealand origin Bovine and Ovine Skins Tanned 
+                with Marine Fish oils in Traditional Processing methods, which helps our Leathers and processing 
+                system to meet sustainability.</p>
 
-                <p className="para3"> New Zealand Origin Bovine and Ovine 
-                Skins Tannned with Marine Fish Oils
-                in Traditional Processing Methods. New Zealand Origin Bovine and Ovine 
-                Skins Tannned with Marine Fish Oils
-                in Traditional Processing Methods. New Zealand Origin Bovine and Ovine 
-                Skins Tannned with Marine Fish Oils
-                in Traditional Processing Methods. New Zealand Origin Bovine and Ovine 
-                Skins Tannned with Marine Fish Oils
-                in Traditional Processing Methods.New Zealand Origin Bovine and Ovine 
-                Skins Tannned</p>
+                <p className="para3"> Our Sheep chamois Leathers are 100% Biodegradable, Antimicrobial, with Natural
+                 Golden color. They are extremely Soft, Luxurious with Drying, and polishing properties for
+                  Automobiles, jewelry, watches, and any screen surface. it has unique Water absorption properties 
+                  of 375 % and above its weight of water and conforms to BS standards.</p>
 
                 <div className="image-section2">
                 <div class="leftimage-div">
                     <img src={leftimg} className="lft-img" />
                 </div>
-                <p className="para4"> New Zealand Origin Bovine and Ovine 
-                Skins Tannned with Marine Fish Oils
-                in Traditional Processing Methods. New Zealand Origin Bovine and Ovine 
-                Skins Tannned with Marine Fish Oils
-                in Traditional Processing Methods. New Zealand Origin Bovine and Ovine 
-                in Traditional Processing Methods.</p>
+                <p className="para4"> SAI offers New Zealand Sheep, Goat, Deer, Hides chamois skins,
+                 and finished Leathers for Gloves, Bags, and Shoes, with Non-Toxic / Anti Cytotoxicity 
+                 properties. We also process Health care Leathers for Orthopedics and Other Medical uses
+                  and General Health Care as per Test standards: ISO 10993-5-2009 (E) Direct Method.</p>
                 </div>
 
-                <p className="para4"> New Zealand Origin Bovine and Ovine 
-                Skins Tannned with Marine Fish Oils
-                in Traditional Processing Methods. New Zealand Origin Bovine and Ovine 
+                <p className="para4"> We process specialty Leathers for Antivirus 
+                Chamois Towels, Glove’s product confirming to TEST STANDARD: ISO 21702:2019*  
                 </p>
 
-                <p className="para4"> New Zealand Origin Bovine and Ovine 
-                Skins Tannned with Marine Fish Oils
-                in Traditional Processing Methods. New Zealand Origin Bovine and Ovine 
-                Skins Tannned with Marine Fish Oils
-                in Traditional Processing Methods.</p>
+                <p className="para4"> SAI’S Care for Environment: Sai Chamois deeply cares about our planet.
+                 All our chamois leathers and Finished Leathers are Biodegradable and Renewable.</p>
 
-                <p className="para4"> New Zealand Origin Bovine and Ovine 
-                Skins Tannned with Marine Fish Oils
-                in Traditional Processing Methods. New Zealand Origin Bovine and Ovine 
-                Skins Tannned with Marine Fish Oils
-                in Traditional Processing Methods. New Zealand Origin Bovine and Ovine 
-                Skins Tannned with Marine Fish Oils
-                in Traditional Processing Methods. New Zealand Origin Bovine and Ovine 
-                Skins Tannned</p>
+                <p className="para4"> 
+All are input skins and Hides are processed from New Zealand origin animals.
+The Sheep, Deer, Goat, and Hides skins are the by-product of the Farming 
+and Meat Industry of New Zealand which has a high reputation for Animal welfare,
+ highest level of ethical standards.
+</p>
 
                 <div className="column-div">
-                <p className="column1"> <span className="column-heading">Certification:</span><br/> New Zealand Origin Bovine and Ovine 
-                Skins Tannned with Marine Fish Oils
-                in Traditional Processing Methods. New Zealand Origin Bovine and Ovine 
-                Skins Tannned with Marine Fish Oils
-                in Traditional Processing Methods. New Zealand Origin Bovine and Ovine 
-                Skins Tannned with Marine Fish Oils
-                in Traditional Processing Methods. New Zealand Origin Bovine and Ovine 
-                Skins Tannned</p>
+                <p className="column1"> <span className="column-heading">Certification:</span><br/> All our 
+                Inputs are monitored with compliance and our Sai Chamois processing plant continues to work in 
+                recycling and follows safe equipment and sustainable processing. 
+Audited by Leather Working Group (LWG) as a GOLD-rated member.
+</p>
 
-                <p className="column2"> <span className="column-heading">Traceability:</span><br/> New Zealand Origin Bovine and Ovine 
-                Skins Tannned with Marine Fish Oils
-                in Traditional Processing Methods. New Zealand Origin Bovine and Ovine 
-                Skins Tannned with Marine Fish Oils
-                in Traditional Processing Methods. New Zealand Origin Bovine and Ovine 
-                Skins Tannned with Marine Fish Oils
-                in Traditional Processing Methods. New Zealand Origin Bovine and Ovine 
-                Skins Tannned</p>
+                <p className="column2"> <span className="column-heading">Traceability:</span><br/>Skins and Hides are Bi 
+                products of New Zealand Farming and Meat Industry wherein Highest level of ethical and animal welfare system 
+                is practiced, Our business presence of more than a decade and with a trusted relationship with well-established
+                ew Zealand Meat companies committed to Animal welfare 
+                and sustainable process systems, helps us able to offer Traceability for our leathers</p>
                 <div className="sideimage-div">
                     <img src={sideimg1} className="sideimage1" />
                     <img src={sideimg2} className="sideimage2" />
